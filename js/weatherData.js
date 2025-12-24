158ee1fb7bb5e5d972dfcc00302acb3e05eb904b
olmaximova <ol.maximova1309@gmail.com>
2025-12-25 01:05:08 +0300

    feat: weather data

---
+++ js/weatherData.js
@@ -0,0 +1,41 @@
+import { CurrentWeather } from './currentWeather.js';
+import { DailyForecast } from './dailyForecast.js';
+
+export class WeatherData {
+    constructor(apiResponse) {
+        this.latitude = apiResponse.latitude;
+        this.longitude = apiResponse.longitude;
+        this.timezone = apiResponse.timezone;
+        
+        if (apiResponse.current) {
+            this.current = new CurrentWeather(apiResponse.current);
+        }
+        
+        this.dailyForecasts = [];
+        if (apiResponse.daily && apiResponse.daily.time) {
+            for (let i = 0; i < apiResponse.daily.time.length; i++) {
+                this.dailyForecasts.push(new DailyForecast(apiResponse.daily, i));
+            }
+        }
+    }
+    
+    get today() {
+        return this.dailyForecasts[0] || null;
+    }
+    
+    get tomorrow() {
+        return this.dailyForecasts[1] || null;
+    }
+    
+    get dayAfterTomorrow() {
+        return this.dailyForecasts[2] || null;
+    }
+    
+    get threeDayForecast() {
+        return this.dailyForecasts.slice(0, 3);
+    }
+    
+    get isValid() {
+        return this.current && this.dailyForecasts.length > 0;
+    }
+}
