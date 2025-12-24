7b642fcee17b0c953d5563714b58f720f4ba65ce
olmaximova <ol.maximova1309@gmail.com>
2025-12-25 02:01:29 +0300

    feat: get daily forecast

---
+++ js/dailyForecast.js
@@ -0,0 +1,15 @@
+export class DailyForecast {
+    constructor(data, index = 0) {
+        this.date = data.time[index];
+        this.maxTemperature = data.temperature_2m_max[index];
+        this.minTemperature = data.temperature_2m_min[index];
+    }
+    
+    get formattedMaxTemperature() {
+        return this.maxTemperature !== undefined ? `${Math.round(this.maxTemperature)}°C` : '—';
+    }
+    
+    get formattedMinTemperature() {
+        return this.minTemperature !== undefined ? `${Math.round(this.minTemperature)}°C` : '—';
+    }
+}
