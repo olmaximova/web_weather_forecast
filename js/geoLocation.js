4766942d036d9c007839621b6aa578e158cc5a61
olmaximova <ol.maximova1309@gmail.com>
2025-12-24 22:49:16 +0300

    feat: geoLocation.js

---
+++ js/geoLocation.js
@@ -0,0 +1,27 @@
+export class GeolocationService {
+    
+    static async getSimpleCoords() {
+        return new Promise((resolve, reject) => {
+            if (!navigator.geolocation) {
+                reject(new Error('Геолокация не поддерживается'));
+                return;
+            }
+            
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    resolve({
+                        lat: position.coords.latitude,
+                        lon: position.coords.longitude
+                    });
+                },
+                (error) => {
+                    reject(new Error('Не удалось получить местоположение'));
+                }
+            );
+        });
+    }
+    
+    static isSupported() {
+        return 'geolocation' in navigator;
+    }
+}
