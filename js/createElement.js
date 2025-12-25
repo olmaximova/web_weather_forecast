6b790de149fab3f378b74164996df83cdf3fc133
olmaximova <ol.maximova1309@gmail.com>
2025-12-25 03:15:58 +0300

    use formatter on create element

--- js/createElement.js
+++ js/createElement.js
@@ -1,27 +1,27 @@
 export function createElement({
-  tag,
-  text = "",
-  className = "",
-  attributes = {},
-  events = {},
-  dataset = {},
+    tag,
+    text = "",
+    className = "",
+    attributes = {},
+    events = {},
+    dataset = {},
 }) {
-  const element = document.createElement(tag);
+    const element = document.createElement(tag);
 
-  if (className) element.className = className;
-  if (text) element.textContent = text;
+    if (className) element.className = className;
+    if (text) element.textContent = text;
 
-  Object.entries(attributes).forEach(([key, value]) => {
-    element.setAttribute(key, value);
-  });
+    Object.entries(attributes).forEach(([key, value]) => {
+        element.setAttribute(key, value);
+    });
 
-  Object.entries(events).forEach(([event, handler]) => {
-    element.addEventListener(event, handler);
-  });
+    Object.entries(events).forEach(([event, handler]) => {
+        element.addEventListener(event, handler);
+    });
 
-  Object.entries(dataset).forEach(([key, value]) => {
-    element.dataset[key] = value;
-  });
+    Object.entries(dataset).forEach(([key, value]) => {
+        element.dataset[key] = value;
+    });
 
-  return element;
+    return element;
 }
