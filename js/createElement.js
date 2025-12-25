b7c688c35406e1fe24e1416ed94577973e677d85
olmaximova <ol.maximova1309@gmail.com>
2025-12-25 03:15:14 +0300

    feat: create element function

---
+++ js/createElement.js
@@ -0,0 +1,27 @@
+export function createElement({
+  tag,
+  text = "",
+  className = "",
+  attributes = {},
+  events = {},
+  dataset = {},
+}) {
+  const element = document.createElement(tag);
+
+  if (className) element.className = className;
+  if (text) element.textContent = text;
+
+  Object.entries(attributes).forEach(([key, value]) => {
+    element.setAttribute(key, value);
+  });
+
+  Object.entries(events).forEach(([event, handler]) => {
+    element.addEventListener(event, handler);
+  });
+
+  Object.entries(dataset).forEach(([key, value]) => {
+    element.dataset[key] = value;
+  });
+
+  return element;
+}
