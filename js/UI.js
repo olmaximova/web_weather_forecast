b8c2c3ce7fadaf8dec9fba84f364d5468c5c58d0
olmaximova <ol.maximova1309@gmail.com>
2025-12-25 14:22:56 +0300

    feat: make forms in ui manager

--- js/UI.js
+++ js/UI.js
@@ -1,77 +1,88 @@
+import { FormCreator } from './formCreator.js';
+
 export class UIManager {
     constructor() {
+        this.makeForms();
         this.getEls();
+        this.bindAct();
+    }
+
+    makeForms() {
+        const city = FormCreator.makeCityForm();
+        this.cityForm = city.form;
+        this.cityInp = city.inp;
+        this.citySugg = city.sugg;
+        this.cityErr = city.err;
+        this.submitCity = city.submitBtn;
+        this.cancelCity = city.cancelBtn;
+        this.cityClose = city.closeBtn;
+
+        const loc = FormCreator.makeLocForm();
+        this.locForm = loc.form;
+        this.locInp = loc.inp;
+        this.locSugg = loc.sugg;
+        this.locErr = loc.err;
+        this.submitLoc = loc.submitBtn;
+        this.retryLoc = loc.retryBtn;
+        this.locClose = loc.closeBtn;
+
+        document.querySelector('.main-content').append(this.cityForm, this.locForm);
     }
 
     getEls() {
         this.container = document.getElementById('weather-container');
-        this.cityForm = document.getElementById('city-form-container');
-        this.locForm = document.getElementById('location-form-container');
         this.loading = document.getElementById('loading-state');
         this.errorBox = document.getElementById('error-state');
         this.errorText = document.getElementById('error-message');
-
-        this.cityInp = document.getElementById('city-input');
-        this.citySugg = document.getElementById('suggestions');
-        this.cityErr = document.getElementById('city-error');
-        this.submitCity = document.getElementById('submit-city-btn');
-        this.cancelCity = document.getElementById('cancel-city-btn');
-        this.addBtn = document.getElementById('add-city-btn');
-
-        this.locInp = document.getElementById('location-city-input');
-        this.locSugg = document.getElementById('location-suggestions');
-        this.locErr = document.getElementById('location-error');
-        this.submitLoc = document.getElementById('submit-location-btn');
-        this.retryLoc = document.getElementById('retry-geolocation-btn');
-
         this.refreshBtn = document.getElementById('refresh-btn');
+        this.addBtn = document.getElementById('add-city-btn');
+        this.changeLocBtn = document.getElementById('change-location-btn');
         this.retryBtn = document.getElementById('retry-btn');
-
-        this.changeLocationBtn = document.getElementById('change-location-btn');
     }
 
-    showLoading() {
+    bindAct() {
+        this.cityClose.onclick = () => this.hideCity();
+        this.locClose.onclick = () => this.hideLocationForm();
+        this.cancelCity.onclick = () => this.hideCity();
+    }
+
+    showLoad() {
         this.loading.classList.remove('hidden');
         this.errorBox.classList.add('hidden');
     }
 
-    hideLoading() {
+    hideLoad() {
         this.loading.classList.add('hidden');
     }
 
-    showError(msg, type) {
-        if (type === 'city') {
-            this.cityErr.textContent = msg;
-        } else if (type === 'loc') {
-            this.locErr.textContent = msg;
-        } else {
+    errorMsg(msg, type) {
+        if (type === 'city') this.cityErr.textContent = msg;
+        else if (type === 'loc') this.locErr.textContent = msg;
+        else {
             this.errorText.textContent = msg;
             this.errorBox.classList.remove('hidden');
         }
     }
 
-    hideError() {
+    hideErr() {
         this.errorBox.classList.add('hidden');
     }
 
-    clearError(type) {
-        if (type === 'city') {
-            this.cityErr.textContent = '';
-        } else if (type === 'loc') {
-            this.locErr.textContent = '';
-        }
+    clearErr(type) {
+        if (type === 'city') this.cityErr.textContent = '';
+        else if (type === 'loc') this.locErr.textContent = '';
     }
 
-    showCityForm() {
+    showCity() {
         this.cityForm.classList.remove('hidden');
         this.cityInp.focus();
     }
 
-    hideCityForm() {
+    hideCity() {
         this.cityForm.classList.add('hidden');
         this.cityInp.value = '';
         this.hideSuggestions();
-        this.clearError('city');
+        this.clearErr('city');
     }
 
     showLocationForm() {
@@ -83,7 +94,7 @@ export class UIManager {
         this.locForm.classList.add('hidden');
         this.locInp.value = '';
         this.hideSuggestions();
-        this.clearError('loc');
+        this.clearErr('loc');
     }
 
     showSuggestions(items, container, callback) {
@@ -115,13 +126,6 @@ export class UIManager {
     }
 
     clearElement(el) {
-        while (el.firstChild) {
-            el.removeChild(el.firstChild);
-        }
-    }
-
-    showChangeLocationForm() {
-        this.clearWeatherContainer(); 
-        this.showLocationForm(); 
+        while (el.firstChild) el.removeChild(el.firstChild);
     }
 }
