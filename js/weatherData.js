7952d8d3ecf960b855c1bc24788519bfea195a41
olmaximova <ol.maximova1309@gmail.com>
2025-12-26 04:32:41 +0300

    use code formatter on weatherData.js

--- js/weatherData.js
+++ js/weatherData.js
@@ -6,11 +6,11 @@ export class WeatherData {
         this.latitude = apiResponse.latitude;
         this.longitude = apiResponse.longitude;
         this.timezone = apiResponse.timezone;
-        
+
         if (apiResponse.current) {
             this.current = new CurrentWeather(apiResponse.current);
         }
-        
+
         this.dailyForecasts = [];
         if (apiResponse.daily && apiResponse.daily.time) {
             for (let i = 0; i < apiResponse.daily.time.length; i++) {
@@ -18,15 +18,15 @@ export class WeatherData {
             }
         }
     }
-    
+
     get todayForecast() {
         return this.dailyForecasts[0] || null;
     }
-    
+
     get threeDayForecast() {
-        return this.dailyForecasts.slice(1, 4);
+        return this.dailyForecasts.slice(1, 5);
     }
-    
+
     get isValid() {
         return this.current && this.dailyForecasts.length > 0;
     }
