4381ffcd69f598698f4b46ab7bf2695be60923fa
olmaximova <ol.maximova1309@gmail.com>
2025-12-27 19:26:34 +0300

    minor change in daily forecast

--- js/dailyForecast.js
+++ js/dailyForecast.js
@@ -3,10 +3,10 @@ import { directions } from "./weather_data.js";
 export class DailyForecast {
     constructor(data, index = 0) {
         this.date = data.time[index];
-        this.maxTemperature = data.temperature_2m_max?.[index];
-        this.minTemperature = data.temperature_2m_min?.[index];
-        this.uvIndexMax = data.uv_index_max?.[index];
-        this.precipProbabilityMax = data.precipitation_probability_max?.[index];
+        this.maxTemperature = data.temperature_2m_max[index];
+        this.minTemperature = data.temperature_2m_min[index];
+        this.uvIndexMax = data.uv_index_max[index];
+        this.precipProbabilityMax = data.precipitation_probability_max[index];
         this.windSpeedMax = data.wind_speed_10m_max[index];
         this.windDirection = data.wind_direction_10m_dominant[index];
     }
